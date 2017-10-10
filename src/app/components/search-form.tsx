68b9ca52402e05df5b338383f86ee90c1066f404
Damian Cugley <pdc@corefiling.com>
2017-10-10 10:06:15 +0100

    Add licensing boilerplate.

--- src/app/components/search-form.tsx
+++ src/app/components/search-form.tsx
@@ -1,5 +1,22 @@
-import { Taxonomy } from '@cfl/bigfoot-search-service';
+/*
+ *  Copyright 2017 CoreFiling S.A.R.L.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import * as React from 'react';
+import { Taxonomy } from '@cfl/bigfoot-search-service';
+
 import TaxonomySearch from './taxonomy-search';
 import SearchResults from './search-results';
 
@@ -12,11 +29,7 @@ export interface SearchFormProps {
 export default function SearchForm({ taxonomies }: SearchFormProps): JSX.Element {
   return (
     <div className='app-SearchForm-rectangle'>
-<<<<<<< HEAD
       {taxonomies ? <TaxonomySearch taxonomies={taxonomies}/> : 'Loading ….'}
-=======
-      <TaxonomySearch taxonomies={props.taxonomies}/>
->>>>>>> 08c1c9d... Misc layout tweaks
       <SearchResults results={[]}/>
     </div>
   );
