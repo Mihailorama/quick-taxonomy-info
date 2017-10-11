3f6edf24d5e579743e54cf5d3bab13f7f2f4a2b2
Damian Cugley <pdc@corefiling.com>
2017-10-11 16:37:41 +0100

    Reimplement taxonomy picker with react-selectize.

--- src/app/components/taxonomy-search.tsx
+++ src/app/components/taxonomy-search.tsx
@@ -16,6 +16,7 @@
 
 import * as classNames from 'classnames';
 import * as React from 'react';
+import { SimpleSelect } from 'react-selectize';
 import { isUndefined } from 'util';
 import { Taxonomy } from '@cfl/bigfoot-search-service';
 
@@ -34,15 +35,46 @@ export interface TaxonomySearchProps {
 export default function TaxonomySearch({ taxonomies, selectedEntryPointId, searchText,
     onTaxonomyEntryPointChange, onSearchTextChange, onSearch }: TaxonomySearchProps): JSX.Element {
   const hasTaxonomy = !isUndefined(selectedEntryPointId);
+
+  const groups = taxonomies.map(({id, name, version}) => ({groupId: id, title: name, version}));
+  const options = taxonomies.map(taxonomy => {
+    return taxonomy.entryPoints.map(({id, name}) => ({
+      groupId: taxonomy.id,
+      value: id,
+      label: `${taxonomy.name} (${taxonomy.version}) › ${name}`}));
+  })
+    .reduce((xs, ys) => xs.concat(ys), []);
+  const selectedOption = selectedEntryPointId ? options.find(x => x.value === selectedEntryPointId) : undefined;
+
   return (
-    <form className='app-TaxonomySearch-container' onSubmit={e => {e.preventDefault(); if (onSearch) { onSearch(); }}}>
+    <form className='app-TaxonomySearch' onSubmit={e => {e.preventDefault(); if (onSearch) { onSearch(); }}}>
       <div className='app-TaxonomySearch-select'>
-        <select required value={selectedEntryPointId || ''} onChange={e => onTaxonomyEntryPointChange(+e.currentTarget.value)}>
-          <option key='none' value='' disabled hidden>Taxonomy</option>
-          {taxonomies.map(t => <optgroup label={t.name}>
-            {t.entryPoints.map(e => <option key={e.id} value={e.id}>{e.name}</option>)}
-          </optgroup>)}
-        </select>
+        <SimpleSelect value={selectedOption}
+          hideResetButton
+          placeholder='Taxonomy'
+          restoreOnBackspace={() => ''}
+          onValueChange={({value}) => onTaxonomyEntryPointChange(+value)}
+          renderGroupTitle={(i, {title, version}) => <div key={i} className='app-TaxonomySearch-taxonomyHeading'>
+            {title} <span className='app-TaxonomySearch-taxonomyVersion'>({version})</span>
+          </div>}
+          renderOption={({value, label}) => <div key={value} className='app-TaxonomySearch-entryPoint'>
+            {label.replace(/^.* \(.*\) › /, '')}
+          </div>}
+          renderValue={({label}) => {
+            const [first, rest] = label.split(/›/, 2);
+            return <span>
+              <span className='app-TaxonomySearch-selectedTaxonomy'>{first}</span>
+              <span className='app-TaxonomySearch-selectedEntryPoint'>{rest}</span>
+            </span>;
+          }}
+          renderNoResultsFound = {() => <div className='app-TaxonomySearch-noMatches'>
+            <div className='app-TaxonomySearch-missingTaxonomyLogo'></div>
+            No results found. <a className='app-TaxonomySearch-noMatches-contactLink'
+                href='https://www.corefiling.com/contactus/' target='_blank'>
+              Contact us</a>
+            <br/> to find your Taxonomy.
+          </div>}
+          groups={groups} options={options}/>
       </div>
       <div className={classNames('app-TaxonomySearch-search', {'app-TaxonomySearch-searchEnabled': hasTaxonomy})}>
         <input type='text' placeholder='Search' value={searchText} onChange={e => onSearchTextChange(e.currentTarget.value)}/>
